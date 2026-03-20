7496ea4cbef9d3fa53a882315a0eb93e27c9efdd
Rayah Jenine D. Paje <rayahjenine.paje@neu.edu.ph>
2026-03-20 04:11:01 +0000

    https://scontent.fmnl4-2.fna.fbcdn.net/v/t39.30808-6/542756850_809668284

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'upload.wikimedia.org',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent.fmnl4-2.fna.fbcdn.net',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
