f0aeb5d159c7966bdae53eb87372fe1f5406d472
rayahpaje <rayahjenine.paje@neu.edu.ph>
2026-03-20 23:37:52 +0800

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-5522485182-71252",
-  "appId": "1:856128636869:web:ff5c521021160c57d26dac",
-  "apiKey": "AIzaSyC-r-SYihwU8zpBGKoPsOp5sG3WStPRs0Y",
-  "authDomain": "studio-5522485182-71252.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "856128636869"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
